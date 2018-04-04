ea95c51ffaa1d407fa6a52c71fe94fefe2925cac
Миннуллина Эльвира <e.minnullina@bars-open.ru>
2018-04-04 12:33:29 +0300

    Соединение с серваком, получаем нужные доки
     public static Server server = new Server(ip, 8080);
    пишем свой ip роутера

--- app/src/main/java/com/example/a25fli/documentsender/DocumentActivity.java
+++ app/src/main/java/com/example/a25fli/documentsender/DocumentActivity.java
@@ -2,6 +2,8 @@ package com.example.a25fli.documentsender;
 
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
@@ -11,6 +13,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+import java.io.IOException;
 
 
 public class DocumentActivity extends Fragment {
@@ -39,5 +54,112 @@ public class DocumentActivity extends Fragment {
                 startActivity(intent);
             }
         });
+        final TextView status = getView().findViewById(R.id.status);
+        final GridLayout gridLayout = getView().findViewById(R.id.grid);
+    
+
+        MainActivity.server.getDocList(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        status.setText("Ошибка подключения!");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull final Response response)
+                    throws IOException {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        status.setText("Успешное подключение!");
+                if (response.code() != 200)
+                    return;
+
+                JsonParser parser = new JsonParser();
+                ResponseBody body = response.body();
+
+                if (body == null)
+                    return;
+
+                        String response_string = null;
+                        try {
+                            response_string = body.string();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        JsonArray responseJson = parser.parse(response_string).getAsJsonArray();
+
+                for (JsonElement element : responseJson) {
+                    JsonObject object = element.getAsJsonObject();
+
+                    TextView textView = new TextView(getContext());
+                    textView.setText(object.get("name").getAsString());
+                    textView.setTextColor(Color.WHITE);
+
+                    gridLayout.addView(textView);
+                }
+
+                    }
+
+                });
+            }
+
+        });
     }
 }
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.history);
+//
+//        final TextView status = findViewById(R.id.status);
+//        final GridLayout gridLayout = findViewById(R.id.grid);
+//
+//        server.getDocList(new Callback() {
+//            @Override
+//            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        status.setText("Ошибка подключения!");
+//                    }
+//                });
+//            }
+//
+//            @Override
+//            public void onResponse(@NonNull Call call, @NonNull Response response)
+//                    throws IOException {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        status.setText("Успешное подключение!");
+//                    }
+//                });
+//
+//                if (response.code() != 200)
+//                    return;
+//
+//                JsonParser parser = new JsonParser();
+//                ResponseBody body = response.body();
+//
+//                if (body == null)
+//                    return;
+//
+//                String response_string = body.string();
+//                JsonArray responseJson = parser.parse(response_string).getAsJsonArray();
+//
+//                for (JsonElement element : responseJson) {
+//                    JsonObject object = element.getAsJsonObject();
+//
+//                    TextView textView = new TextView(getApplicationContext());
+//                    textView.setText(object.get("name").getAsString());
+//                    textView.setTextColor(Color.BLACK);
+//
+//                    gridLayout.addView(textView);
+//                }
+//            }
+//        });
+//    }
--- app/src/main/java/com/example/a25fli/documentsender/HistoryActivity.java
+++ app/src/main/java/com/example/a25fli/documentsender/HistoryActivity.java
@@ -31,7 +31,6 @@ import okhttp3.ResponseBody;
 import static android.content.Context.MODE_PRIVATE;
 
 public class HistoryActivity extends Fragment {
-    private Server server = new Server("192.168.8.103", 8080);
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -66,56 +65,4 @@ public class HistoryActivity extends Fragment {
             }
         });
     }
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.history);
-//
-//        final TextView status = findViewById(R.id.status);
-//        final GridLayout gridLayout = findViewById(R.id.grid);
-//
-//        server.getDocList(new Callback() {
-//            @Override
-//            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        status.setText("Ошибка подключения!");
-//                    }
-//                });
-//            }
-//
-//            @Override
-//            public void onResponse(@NonNull Call call, @NonNull Response response)
-//                    throws IOException {
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        status.setText("Успешное подключение!");
-//                    }
-//                });
-//
-//                if (response.code() != 200)
-//                    return;
-//
-//                JsonParser parser = new JsonParser();
-//                ResponseBody body = response.body();
-//
-//                if (body == null)
-//                    return;
-//
-//                String response_string = body.string();
-//                JsonArray responseJson = parser.parse(response_string).getAsJsonArray();
-//
-//                for (JsonElement element : responseJson) {
-//                    JsonObject object = element.getAsJsonObject();
-//
-//                    TextView textView = new TextView(getApplicationContext());
-//                    textView.setText(object.get("name").getAsString());
-//                    textView.setTextColor(Color.BLACK);
-//
-//                    gridLayout.addView(textView);
-//                }
-//            }
-//        });
-//    }
 }
